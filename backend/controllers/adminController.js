b35abefd9b6162e101abe20ea1d83147c591ac32
Ashish <chauhanashish0831@gmail.com>
2026-03-06 11:20:51 +0530

    feat: add admin panel structure

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,83 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import { cloudinary } from '../config/cloudinary.js';
+import doctorModel from '../models/doctorModel.js';
+import jwt from 'jsonwebtoken';
+
+
+// Api for admin doctor
+const addDoctor = async (req, res) => {
+    try {
+        const {name, email, password, speciality, degree, experience, about, fees, address,available}= req.body;
+        const imageFile = req.file;
+
+        // checking for all data to add doctor
+        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address || !imageFile || !available){
+            return res.json({success:false, message:"All fields are required"});
+        }
+
+        // validating email format
+        if(!validator.isEmail(email)){
+            return res.json({success:false, message:"Invalid email format"});
+        }
+
+            // validating password strength
+        if(password.length<8){
+            return res.json({success:false, message:"Password should be at least 8 characters long"});
+        }
+
+        // hashing doctor password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+            resource_type:'image'
+        });
+        const imageUrl = imageUpload.secure_url;
+
+
+        const doctorData = {
+            name,
+            email,
+            password: hashedPassword,
+            image: imageUrl,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            available:true,
+            address: JSON.parse(address),
+            date: Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData);
+        await newDoctor.save();
+
+        res.json({success:true, message:"Doctor added successfully"});
+       
+    } catch (error) {
+        console.log("ERROR:", error);
+        res.json({success:false, message:error.message});
+    }
+}
+
+// API for admin Login
+const loginAdmin = async(req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({success:true, message:"login successful",token});
+        }else{
+            res.json({success:false, message:"Invalid email or password"});
+        }
+    }catch(error){
+        console.log("ERROR:", error);
+        res.json({success:false, message:error.message});
+    }
+}
+
+export { addDoctor, loginAdmin };
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
-import connectCloudinary from './config/cloudinary.js';
+import { connectCloudinary } from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 
 // app config
@@ -20,6 +21,9 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoint
+
+app.use('/api/admin', adminRouter);
+
 app.get('/',(req,res)=>{
     res.send("API WORKING");
 });
